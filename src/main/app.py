ca102e150ba821b6440b8e22d70a57d5f6b83cbb
ravaldhrumil <146651471+ravaldhrumil@users.noreply.github.com>
2024-03-18 12:32:50 +0530

    update

--- src/main/app.py
+++ src/main/app.py
@@ -7,8 +7,6 @@ from Config.config import *
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Models.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -17,8 +15,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 db.init_app(app)
-
-
 
 app.template_folder = "F:\\Open AI integration V1\\src\\templates"
 app.static_folder = "F:\\Open AI integration V1\\src\\static"
